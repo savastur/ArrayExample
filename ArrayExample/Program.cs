e4d83ab6cf2258dfc9ac1c0fa5f7a1db9eef63db
Sturm <savagesturm@gmail.com>
2026-01-28 08:23:53 -0700

    Add project files.

---
+++ ArrayExample/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int thing = 0;
+            string[] fruits = new string[7];
+            fruits[0] = "apple";
+            fruits[1] = "bananna";
+            fruits[2] = "kiwi";
+            fruits[3] = "tomato";
+            fruits[4] = "pineapple";
+            fruits[5] = "dragon fruit";
+            fruits[6] = "watermellon";
+            //fruits[7] = "tangerine"; out of range causes it to crash
+
+            Console.WriteLine(fruits[3]);
+
+
+            
+            
+            //pause
+            Console.Read();
+        }
+    }
+}
